51bc9bd91f5db9346978425868eed344c95f2c68
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-11-13 23:49:30 +0530

    create food order app

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Cart.module.css";
+
+const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12 }];
+
+const Cart = () => {
+  return (
+    <div>
+      <ul className={classes["cart-items"]}>
+        {cartItems.map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
+
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
